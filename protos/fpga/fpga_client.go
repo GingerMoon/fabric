becc024ad32196e90740a593fdfdabfe57152196
accelor2018 <xiaohan@mxh-THINK.localdomain>
2018-12-11 15:18:52 -0800

    Updated fpga client

--- protos/fpga/fpga_client.go
+++ protos/fpga/fpga_client.go
@@ -1,4 +1,33 @@
+package fpga
 
 import (
     "fmt"
+    "context"
+
+    "google.golang.org/grpc"
 )
+
+
+const (
+    address = "localhost:50051"
+    defaultName = "accelor"
+)
+
+
+func fpgaService() {
+    conn, err := grpc.Dail(address, grpc.WithInsecure())
+    if err != nil {
+        log.Fatalf("did not connect: %v", err)
+    }
+
+    defer conn.close()
+
+    c := pb.NewBlockRPCClient(conn)
+
+    name := defaultName
+	
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+    defer cancel()
+    r, err := c.SendBlockData(ctx, &pb.BlockRequest{Name: name})
+}
