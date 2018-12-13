580a68c13f38d56036bfe08cf6f221229e5876da
accelor2018 <xiaohan@mxh-THINK.localdomain>
2018-12-13 15:17:00 -0800

    Updated client functions for fpga grpc services

--- protos/fpga/fpga_client.go
+++ protos/fpga/fpga_client.go
@@ -13,8 +13,52 @@ const (
     defaultName = "accelor"
 )
 
+int blocksize = 10
+uint64 blockid = 0
+uint64 txid = 0
 
-func fpgaService() {
+func SendBlockSize4vscc(BlockDataSize4vscc size) {
+    blocksize = size
+}
+
+func VerifySig4vscc(VsccEnvelope *env) {
+    BlockRequest bq
+
+    bq.set_block_id(blockid++)
+    bq.set_cold_start(0)
+    bq.set_tx_count(blocksize)
+    
+    {
+        BlockRequest_Transaction* tx = bq.add_tx()
+        tx->set_tx_id(txid++)
+    }
+
+    bq.set_crc(0x1234)
+
+    r, err := fpgaService(blockRequest)
+    return r
+}
+
+func SendBlock4mvcc(Block4mvcc *block) {
+    BlockRequest bq
+
+    bq.set_block_id(blockid++) // ask if fabrc could provide one
+    bq.set_cold_start(0) // cold start is coming with write-only data
+    bq.set_tx_count(block->num) // num is txCount?
+
+    {
+        BlockRequest_Transacton* tx = bq.add_tx()
+        tx->set_tx_id(txid++) // ask if fabrc could provde one
+    }
+
+    bq.set_crc(0x1234) // compute crc here? or in drver ?   
+
+    r, err := fpgaService(bq)
+    return r
+}
+
+// Send blockRequest t FGPA driver
+func fpgaService(BlockRequest block) {
     conn, err := grpc.Dail(address, grpc.WithInsecure())
     if err != nil {
         log.Fatalf("did not connect: %v", err)
@@ -30,4 +74,6 @@ func fpgaService() {
 
     defer cancel()
     r, err := c.SendBlockData(ctx, &pb.BlockRequest{Name: name})
+
+    return r
 }
