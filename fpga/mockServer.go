82faa78bc383e409a6b4bf206ad14246d02bd6aa
accelor2018 <maxiaohan@gmail.com>
2018-12-25 14:26:12 +0800

    Added logger ostream

--- fpga/mockServer.go
+++ fpga/mockServer.go
@@ -41,25 +41,69 @@ func (s *fpgaServer) VerifySig4Vscc(cxt context.Context, env *fpga.VsccEnvelope)
 		logger.Warnf("grpc server verify result: false")
 	}
 
-/*
         // compose block_rpc data structure
         BlockRequest bq
+        bq.BlockId = 0
+        bq.ColdStart = 0
+        bq.TxCount = 1
+
+        txs := make([]*BlockRequest_Transaction, 0, 1)
+        txs[0].TxId = ""
+        txs[0].RdCount = 0
+        txs[0].WtCount = 0
+        txs[0].SgCount = 1
+        txs[0].DbgTxValid = false
+        
+        BlockRequest_Transaction_SignatureStruct sig
+        sig.SignR = intR
+        sig.SignW = intS
+        sig.PkX = x
+        sig.PkY = y
+        sig.E = digest
+
+        txs[0].Signatures = sig
+        bq.Transaction = txs
+        bq.Crc = 0x1234
 
         // serialize to stream
         data, err := proto.Marshal()
         if err != nil {
             log.Fatalln("Marshal data error:", err)
         }
-*/
-	return &fpga.VsccResponse{Result:valid}, nil
+	
+        return &fpga.VsccResponse{Result:valid}, nil
 }
 
-func (s *fpgaServer) SendBlock4Mvcc(context.Context, *fpga.Block4Mvcc) (*fpga.MvccResponse, error) {
+func (s *fpgaServer) SendBlock4Mvcc(cxt context.Context, env *fpga.Block4Mvcc) (*fpga.MvccResponse, error) {
 	logger.Infof("mock fpga  receive a SendBlock4Mvcc request")
         
         // compose block_rpc data structure
         BlockRequest bq
-       
+        bq.BlockId = 0
+        bq.ColdStart = 1
+        bq.TxCount = env.num
+
+        txs := make([]*BlockRequest_Transaction, 0, bq.TxCount)
+        for i := 0; i < bq.TxCount; i++ {
+            rd := make([]*BlockRequest_Transaction_ReadStruct, 0, 1)
+            rd[0].Key = env.Txs[i].Rs[0].Key
+            rd[0].Version = env.Txs[i].Rs[0].Version
+
+            wt := make([]*BlockRequest_Transaction_WriteStruct, 0, 1)
+            wt[0].Key = env.Txs[i].Rs[0].Key
+            wt[0].Version = env.Txs[i].Rs[0].Version
+            wt[0].IsDel = env.Txs[i].Rs[0].IsDel
+
+            txs[i].IndexInBlock = env.Txs[i].IndexInBlock
+            txs[i].Id = env.Txs[i].Id
+            txs[i].RdCount = 1
+            txs[i].WtCount = 1
+            txs[i].Reads = rd
+            txs[i].Writes = wt
+        }
+        
+        bq.Transaction = txs
+        bq.Crc = 0x1234
 
         // serialize to stream
         data, err := proto.Marshal(bq)
