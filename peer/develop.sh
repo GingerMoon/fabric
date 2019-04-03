169ef901b0f06f599da24f400ff915042edfc957
wangxiang <wangxiang19870507@gmail.com>
2019-04-03 17:08:29 +0800

    1. refined the develop.sh by closing the fabric network before updating peer.

--- peer/develop.sh
+++ peer/develop.sh
@@ -1,4 +1,7 @@
 env GOOS=linux GOARCH=amd64 go build
+cd /c/goworkspace/src/bitbucket.org/accelorteam/demo/fabric-network/first-network
+./byfn.sh down
+cd /c/goworkspace/src/github.com/hyperledger/fabric/peer
 mv peer /c/goworkspace/src/bitbucket.org/accelorteam/demo/fabric-network/bin
 cd /c/goworkspace/src/bitbucket.org/accelorteam/demo/fabric-network/first-network
 ./debug.sh
