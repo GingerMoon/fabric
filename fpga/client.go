4303cbc8a77b36678094be741889bd6317ef41a6
wangxiang <wangxiang19870507@gmail.com>
2019-04-13 14:45:42 +0800

    1. added a channel for listen the signal from the system.

--- fpga/client.go
+++ fpga/client.go
@@ -4,15 +4,24 @@ import (
 	"github.com/hyperledger/fabric/common/flogging"
 	"google.golang.org/grpc"
 	"os"
+	"os/signal"
 )
 
 var (
+	sigs chan os.Signal
 	logger = flogging.MustGetLogger("fpga")
 	serverAddr = os.Getenv("FPGA_SERVER_ADDR")
 	conn *grpc.ClientConn
 )
 
 func init() {
+	sigs = make(chan os.Signal, 1)
+	signal.Notify(sigs)
+	go func() {
+		sig := <-sigs
+		logger.Fatalf("-------------received a signal from the system: %v", sig)
+	}()
+
 	logger.Infof("FPGA_MOCK: ", os.Getenv("FPGA_MOCK"))
 	logger.Infof("FPGA_SERVER_ADDR: ", serverAddr)
 	logger.Infof("FPGA_BATCH_GEN_INTERVAL: (microsecond)", os.Getenv("FPGA_BATCH_GEN_INTERVAL"))
