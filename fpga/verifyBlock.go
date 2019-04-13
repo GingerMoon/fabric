63a443a86ebc040f2847e4ab0026c6e75fb78357
wangxiang <wangxiang19870507@gmail.com>
2019-04-13 09:30:25 +0800

    1. when invalid signature found in the rpc verify batch reply, verify it via go std verify. and exit if it's valid according to go std library.

--- fpga/verifyBlock.go
+++ fpga/verifyBlock.go
@@ -1,9 +1,12 @@
 package fpga
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	commonerrors "github.com/hyperledger/fabric/common/errors"
 	pb "github.com/hyperledger/fabric/protos/fpga"
 	"github.com/pkg/errors"
+	"math/big"
 )
 
 func CommitBlockVerify(svRequests []*pb.BatchRequest_SignVerRequest) error {
@@ -20,8 +23,33 @@ func CommitBlockVerify(svRequests []*pb.BatchRequest_SignVerRequest) error {
 		verifyResults := reply.SvReplies
 		for i, result := range verifyResults {
 			if !result.Verified {
+				logger.Errorf("the batch containing the invalid signature is: %v", *in)
+				logger.Errorf("the signature is invalid verified by FPGA. batchId: %d, reqId: %d", reply.BatchId, result.ReqId)
+
+				for _, req := range svRequests {
+					if req.ReqId != result.ReqId {
+						continue
+					}
+
+					r := &big.Int{}
+					r.SetBytes(req.SignR)
+					s := &big.Int{}
+					s.SetBytes(req.SignS)
+
+					x := &big.Int{}
+					x.SetBytes(req.Px)
+					y := &big.Int{}
+					y.SetBytes(req.Py)
+					pubkey := ecdsa.PublicKey{Curve:elliptic.P256(), X:x, Y:y}
+					succeed := ecdsa.Verify(&pubkey, req.Hash[:], r, s)
+					if succeed {
+						logger.Fatalf("The signature is invalid [verified by FPGA], but it is valid verified by go library.")
+					}
+					return nil
+				}
+
 				return &commonerrors.VSCCEndorsementPolicyError {
-					Err: errors.Errorf("CommitBlockVerify (Endorsement or CheckCreator) failed. internal index: %d. ", i), // TODO the index(i) is not accurate.
+					Err: errors.Errorf("CommitBlockVerify failed. internal index: %d. ", i), // TODO the index(i) is not accurate.
 				}
 			}
 		}
