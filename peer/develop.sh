32bf6a7bd0a6539385b2c8ed3d1b191196966037
wangxiang <wangxiang19870507@gmail.com>
2019-04-15 16:56:45 +0800

    1. change proto version from 3 to 2. 2. refine the develop.sh

--- peer/develop.sh
+++ peer/develop.sh
@@ -1,6 +1,7 @@
 env GOOS=linux GOARCH=amd64 go build
 cd /c/goworkspace/src/bitbucket.org/accelorteam/demo/fabric-network/first-network
 ./byfn.sh down
+rm -f /c/goworkspace/src/bitbucket.org/accelorteam/demo/fabric-network/bin/peer
 cd /c/goworkspace/src/github.com/hyperledger/fabric/peer
 mv peer /c/goworkspace/src/bitbucket.org/accelorteam/demo/fabric-network/bin
 cd /c/goworkspace/src/bitbucket.org/accelorteam/demo/fabric-network/first-network
